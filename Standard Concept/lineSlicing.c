038614b1b4f1642c2be345dc530cf79ac7d685f6
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-04 12:12:14 +0530

    Line Splicing

---
+++ Standard Concept/lineSlicing.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    //Line Spacing - New Line
+    printf("Hello Line SLicer\n");
+    printf("Welcome\n");
+
+    //Tab Space
+    printf("Hello \t");
+    printf("Slicer\n");
+
+    // r - read and returns the character
+    printf("Hello \r");
+    printf("Slicer");
+}
