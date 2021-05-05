4d3020dce0fcb86891dd714530ec30922c53ca04
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-06 01:44:24 +0530

    Math

---
+++ Standard Concept/Math/math.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("\n%f",ceil(3.6));
+    printf("\n%f",floor(3.3));
+    printf("\n%f",sqrt(16));
+    printf("\n%f",pow(2,3));
+    printf("\n%d",abs(-12));
+}
