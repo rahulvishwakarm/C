f6a60d186e43957cd2f7d418bfc676b7f3b9e92d
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-18 00:19:23 +0530

    Memory Allocation - calloc() updated

--- Data Structures/Linked List/Memory Allocation/calloc.c
+++ Data Structures/Linked List/Memory Allocation/calloc.c
@@ -6,3 +6,26 @@
 // 2. Memory alocated by calloc is initialized to zero
 
 // calloc stands for clear allocation and malloc stands for memory allocation
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter the number of Integer: ");
+    scanf("%d",&n);
+    int *ptr = (int *)calloc(n,sizeof(int)); // if sucess it will allocate size else will return null
+    if(ptr==NULL) {
+        printf("Memory not available or memory allocation failed!!!");
+    }
+    for(i=0;i<n;i++) {
+        printf("Enter an Integer: ");
+        scanf("%d",ptr+i);
+    }
+    for(i=0;i<n;i++) {
+        printf("%d\t",*(ptr+i));
+        printf("%d\t",(ptr+i));
+    }
+    return 0;
+}
