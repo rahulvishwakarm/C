94a718303a86e7fca886f06b224d3132efff6143
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-27 21:22:48 +0530

    Doubly Linked List - Deletion From End

---
+++ Data Structures/Linked List/Doubly Linked List/DeleteFromEnd.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+struct Node *last = NULL;
+struct Node *current = NULL;
+
+void insert(int d)
+{
+    struct Node *link = malloc(sizeof(struct Node));
+    link->data = d;
+    link->prev = NULL;
+    link->next = NULL;
+
+    if(head == NULL)
+    {
+        head = link;
+        return;
+    }
+    current = head;
+
+    while (current->next!=NULL)
+    {
+        current = current->next;
+    }
+    current->next = link;
+    last = link;
+    link->prev = current;
+};
+
+void deletefromEnd()
+{ 
+    struct Node *deleteptr = head;
+    while (deleteptr->next!=NULL)
+    {
+        deleteptr = deleteptr->next;
+        if(deleteptr->next->next==NULL)
+        {
+            deleteptr->next = NULL;
+        }
+    }
+};
+
+void printList()
+{
+    struct Node *ptr = head;
+    printf("[head]");
+    while (ptr->next!=NULL)
+    {
+        printf("<=>%d",ptr->data);
+        ptr = ptr->next;
+    }
+    printf("<=>%d",ptr->data);
+    printf("<=>[head]\n");
+};
+
+int main()
+{
+    insert(11);
+    insert(22);
+    insert(33);
+    insert(44);
+    insert(55);
+    printList();
+    deletefromEnd();
+    printList();
+}
