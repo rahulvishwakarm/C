0f33b05691a4345b36f32d7507cc9a94fd43baa5
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-06-01 02:59:13 +0530

    Circular Singly Linked List - Insertion At End

---
+++ Data Structures/Linked List/Circular Linked List/Circular Singly Linked List/InsertAtEnd.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *InsertInEmpty(struct Node *head,int data)
+{
+    if(head!=NULL)
+    {
+        return head;
+    };
+    struct Node *lnk = malloc(sizeof(struct Node));
+    lnk->data = data;
+    head = lnk;
+    lnk->next = head;
+    return head;
+};
+
+struct Node *InsertAtBeginning(struct Node *head,int data)
+{
+    if(head == NULL)
+    {
+        return InsertInEmpty(head,data);
+    }
+    struct Node *lnk = malloc(sizeof(struct Node));
+    lnk->data = data;
+    lnk->next=head->next;
+    head->next=lnk;
+    return head;
+};
+
+struct Node *InsertAtEnd(struct Node *head,int data)
+{
+    struct Node *lnk = malloc(sizeof(struct Node));
+    lnk->data = data;
+    lnk->next = head->next;
+    head->next = lnk;
+    head = lnk;
+    return head;
+};
+
+void travserse(struct Node *head)
+{
+    if(head==NULL)
+    {
+        printf("Linked list is Empty!!");
+    }
+    struct Node *ptr = head->next;
+    printf("[head]->");
+    do
+    {
+        printf("%d->",ptr->data);
+        ptr = ptr->next;
+    } while (ptr!=head->next);
+    printf("[head]");
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    head = InsertInEmpty(head,22);
+    head = InsertAtBeginning(head,11);
+    head = InsertAtEnd(head,33);
+    travserse(head);
+}
