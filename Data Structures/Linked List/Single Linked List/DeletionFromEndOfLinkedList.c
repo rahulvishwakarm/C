1f30d48fafa0d9ca55bd79b5727b290d07f78e57
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-07-02 03:49:50 +0530

    Single Linked List - Deletion From End Updated

--- Data Structures/Linked List/Single Linked List/DeletionFromEndOfLinkedList.c
+++ Data Structures/Linked List/Single Linked List/DeletionFromEndOfLinkedList.c
@@ -1,60 +1,125 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node {
+//Creating the node
+struct Node
+{
     int data;
     struct Node *next;
 };
 
+struct Node *head = NULL;
+
+struct Node *NodeCreation(int data)
+{
+    struct Node *Nodes = (struct Node*) malloc(sizeof(struct Node));
+    Nodes->data = data;
+    Nodes->next = head;
+    head = Nodes;
+}
+
+struct Node *InsertioAtBeginning(int data)
+{
+    struct Node *begNode = (struct Node*) malloc(sizeof(struct Node));
+    begNode->data = data;
+    begNode->next = head;
+    head = begNode;
+}
+
+struct Node *InsertioAtSpecificPosition(int data,int pos)
+{
+    struct Node *specificNode = (struct Node*) malloc(sizeof(struct Node));
+    specificNode->data = data;
+    struct Node *ptr = head;
+    for(int i=1;i<pos;i++) {
+        ptr = ptr->next;
+        if(ptr==NULL)
+        {
+            printf("Insertion is not possible!!\n");
+            printf("Reached at the end of the list!!");
+        }
+    }
+    specificNode->next = ptr->next;
+    ptr->next = specificNode;
+}
+
+struct Node *LastNodeInsertion(int data)
+{
+    struct Node *lastInsert = (struct Node*) malloc(sizeof(struct Node));
+    lastInsert->data = data;
+    lastInsert->next = NULL;
+    struct Node *ptr = head;
+    while (ptr->next!=NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = lastInsert;
+}
+
+struct Node *DeleteFromBeginning()
+{
+    if(head==NULL) {
+        printf("Linked List is empty!!");
+    }
+    struct Node *deltptr = head;
+    head = head->next;
+    free(deltptr);
+    deltptr = NULL;
+}
+
+struct Node *DeleteFromSpecificPosition(int pos)
+{
+    struct Node *deltptr;
+    struct Node *ptr = head;
+    for(int i=1;i<pos-1;i++) {
+        ptr = ptr->next;
+    }
+    deltptr = ptr->next->next;
+    free(ptr->next);
+    ptr->next = deltptr;
+}
+
+struct Node *DeleteFromEnd()
+{
+    struct Node *deltptr;
+    struct Node *ptr = head;
+    while (ptr->next!=NULL)
+    {
+        deltptr = ptr;
+        ptr = ptr->next;
+    }
+    deltptr->next = NULL;
+}
+
+struct Node *TraverseList()
+{
+    struct Node *ptr = head;
+    printf("[head]=>");
+    while (ptr!=NULL)
+    {
+        printf("%d=>",ptr->data);
+        ptr = ptr->next;
+    }
+    printf("[NULL]\n");
+}
+
 int main()
 {
-    struct Node *head = malloc(sizeof(struct Node));
-    struct Node *second = malloc(sizeof(struct Node));
-    struct Node *third = malloc(sizeof(struct Node));
-    struct Node *fourth = malloc(sizeof(struct Node));
-
-    head->data = 11;
-    head->next = NULL;
-
-    second->data = 22;
-    second->next = NULL;
-
-    third->data = 33;
-    third->next = NULL;
-
-    fourth->data = 44;
-    fourth->next = NULL;
-
-    third->data = 33;
-    third->next = NULL;
-
-    head->next = second;
-    second->next = third;
-    third->next = fourth;
-
-    // Traversing the array
-    struct Node *ptr1 = head;
-    while(ptr1!=NULL) {
-        printf("%d\t",ptr1->data);
-        ptr1 = ptr1->next;
-    }
-
-    // Deleting Node
-    struct Node *pos = head;
-    struct Node *ps;
-    while(pos->next!=NULL) {
-        ps = pos;
-        pos = pos->next;
-    }
-    ps->next=NULL;
-    free(pos);
-
-    printf("\n");
-
-    // Traversing the array
-    struct Node *ptr2 = head;
-    while(ptr2!=NULL) {
-        printf("%d\t",ptr2->data);
-        ptr2 = ptr2->next;
-    }
+    NodeCreation(10);
+    NodeCreation(20);
+    NodeCreation(30);
+    NodeCreation(40);
+    TraverseList();
+    InsertioAtBeginning(50);
+    TraverseList();
+    InsertioAtSpecificPosition(32,2);
+    TraverseList();
+    LastNodeInsertion(9);
+    TraverseList();
+    DeleteFromBeginning();
+    TraverseList();
+    DeleteFromSpecificPosition(2);
+    TraverseList();
+    DeleteFromEnd();
+    TraverseList();
 }
