2428d112b6d1d231289eca02ee3c5a6e7b3aa80e
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-07-02 03:48:35 +0530

    Single Linked List - Deletion From Specific Position Updated

--- Data Structures/Linked List/Single Linked List/DeletionFromSpecificPosition.c
+++ Data Structures/Linked List/Single Linked List/DeletionFromSpecificPosition.c
@@ -1,74 +1,111 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Creating the node
 struct Node
 {
     int data;
     struct Node *next;
 };
 
+struct Node *head = NULL;
+
+struct Node *NodeCreation(int data)
+{
+    struct Node *Nodes = (struct Node*) malloc(sizeof(struct Node));
+    Nodes->data = data;
+    Nodes->next = head;
+    head = Nodes;
+}
+
+struct Node *InsertioAtBeginning(int data)
+{
+    struct Node *begNode = (struct Node*) malloc(sizeof(struct Node));
+    begNode->data = data;
+    begNode->next = head;
+    head = begNode;
+}
+
+struct Node *InsertioAtSpecificPosition(int data,int pos)
+{
+    struct Node *specificNode = (struct Node*) malloc(sizeof(struct Node));
+    specificNode->data = data;
+    struct Node *ptr = head;
+    for(int i=1;i<pos;i++) {
+        ptr = ptr->next;
+        if(ptr==NULL)
+        {
+            printf("Insertion is not possible!!\n");
+            printf("Reached at the end of the list!!");
+        }
+    }
+    specificNode->next = ptr->next;
+    ptr->next = specificNode;
+}
+
+struct Node *LastNodeInsertion(int data)
+{
+    struct Node *lastInsert = (struct Node*) malloc(sizeof(struct Node));
+    lastInsert->data = data;
+    lastInsert->next = NULL;
+    struct Node *ptr = head;
+    while (ptr->next!=NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = lastInsert;
+}
+
+struct Node *DeleteFromBeginning()
+{
+    if(head==NULL) {
+        printf("Linked List is empty!!");
+    }
+    struct Node *deltptr = head;
+    head = head->next;
+    free(deltptr);
+    deltptr = NULL;
+}
+
+struct Node *DeleteFromSpecificPosition(int pos)
+{
+    struct Node *deltptr;
+    struct Node *ptr = head;
+    for(int i=1;i<pos-1;i++) {
+        ptr = ptr->next;
+    }
+    deltptr = ptr->next->next;
+    free(ptr->next);
+    ptr->next = deltptr;
+}
+
+struct Node *TraverseList()
+{
+    struct Node *ptr = head;
+    printf("[head]=>");
+    while (ptr!=NULL)
+    {
+        printf("%d=>",ptr->data);
+        ptr = ptr->next;
+    }
+    printf("[NULL]\n");
+}
+
 int main()
 {
-    struct Node *head = malloc(sizeof(struct Node));
-    struct Node *second = malloc(sizeof(struct Node));
-    struct Node *third = malloc(sizeof(struct Node));
-    struct Node *fourth = malloc(sizeof(struct Node));
-    struct Node *fifth = malloc(sizeof(struct Node));
-
-    head->data = 11;
-    head->next = NULL;
-
-    second->data = 22;
-    second->next = NULL;
-
-    third->data = 33;
-    third->next = NULL;
-
-    fourth->data = 44;
-    fourth->next = NULL;
-
-    fifth->data = 55;
-    fifth->next = NULL;
-
-    head->next = second;
-    second->next = third;
-    third->next = fourth;
-    fourth->next = fifth;
-
-    // Traversing the List
-    struct Node *ptr1 = head;
-    while (ptr1!=NULL)
-    {
-        printf("%d\t",ptr1->data);
-        ptr1 = ptr1->next;
-    }
-
-    printf("\n");
-
-    // Deleting the List
-    struct Node *point = head;
-    struct Node *pnt;
-
-    int NTOD;
-    printf("Which Node to delete?: ");
-    scanf("%d",&NTOD);
-
-    // Deleting
-    for(int i=1;point!=NULL && i<NTOD-1;i++) {
-        point = point->next;
-    }
-    if(point==NULL || point->next==NULL) {
-        printf("Node Not found, You are deleting last node!!");
-    }
-    pnt = point->next->next; //holding node next to node to be deleted so that it can be accessed
-    free(point->next);
-    point->next = pnt;
-
-    // Traversing the List
-    struct Node *ptr2 = head;
-    while (ptr2!=NULL)
-    {
-        printf("%d\t",ptr2->data);
-        ptr2 = ptr2->next;
-    }
+    NodeCreation(10);
+    NodeCreation(20);
+    NodeCreation(30);
+    NodeCreation(40);
+    TraverseList();
+    InsertioAtBeginning(50);
+    TraverseList();
+    InsertioAtSpecificPosition(32,2);
+    TraverseList();
+    LastNodeInsertion(9);
+    TraverseList();
+    DeleteFromBeginning();
+    TraverseList();
+    DeleteFromSpecificPosition(2);
+    TraverseList();
 }
